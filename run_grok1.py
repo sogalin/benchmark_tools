a49e3d467ca0a15241256fe0eb20f8aebb3efc64
Matthew Wong <Matthew.Wong2@amd.com>
2025-06-24 01:44:01 +0000

    Revise run_grok1.py for new SGLang.

--- run_grok1.py
+++ run_grok1.py
@@ -3,12 +3,15 @@ import torch
 
 # Settings
 batch_sizes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
-inputs = [128, 2048]
-outputs = [128, 2048]
-tp_values = [8, 4]  # Default TP values
+#inputs = [128, 2048]
+#outputs = [128, 2048]
+inputs = [256]
+outputs = [2048]
+tp_values = [8]  # Default TP values
 
 # Define the model path
-model_path = "/sglang/dummy_grok1/"
+#model_path = "/data/models/huggingface/amd--grok-1-W4A8KV8"
+model_path = "/data/grok-1-W4A8KV8"
 # Extract model name from the model path
 model_name = model_path.split('/')[-2]
 
@@ -29,15 +32,17 @@ for tp in tp_values:
 
                 # Construct the command to execute
                 command = (
-                    f"HIP_FORCE_DEV_KERNARG=1 python -m sglang.bench_latency "
-                    f"--model {model_path} "
-                    f"--load-format dummy "
-                    f"--tokenizer-path Xenova/grok-1-tokenizer "
-                    f"--tp {tp} "
+                    f"RCCL_MSCCL_ENABLE=0 SGLANG_AITER_MOE=1 SGLANG_INT4_WEIGHT=1 "
+                    f"python -m sglang.bench_one_batch "
                     f"--batch-size {batch_size} "
                     f"--input {input_size} "
                     f"--output {output_size} "
-                    f"--quant fp8 "
+                    f"--model {model_path} "
+                    f"--tokenizer-path Xenova/grok-1-tokenizer "
+                    f"--tp {tp} "
+                    f"--quantization fp8 "
+                    f"--trust-remote-code "
+                    f"--attention-backend aiter"
                 )
 
                 # Command to write both the executed command and all output (stdout and stderr) to the output_file
